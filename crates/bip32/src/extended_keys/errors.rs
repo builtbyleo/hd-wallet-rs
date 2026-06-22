4bb9710e28b3dc03fb64ebc37d2c3f3e744c39aa
builtbyleo <leonardpaturel@proton.me>
2026-06-22 11:36:49 +0200

    ref: move errors

---
+++ crates/bip32/src/extended_keys/errors.rs
@@ -0,0 +1,22 @@
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[non_exhaustive]
+pub enum Error {
+    /// Opaque crypto errors
+    Crypto,
+    /// Max Depth reached
+    MaxDepth,
+    /// Invalid Range
+    InvalidRange,
+}
+
+impl From<k256::ecdsa::Error> for Error {
+    fn from(_: k256::ecdsa::Error) -> Error {
+        Error::Crypto
+    }
+}
+
+impl From<sha2::digest::InvalidLength> for Error {
+    fn from(_: sha2::digest::InvalidLength) -> Error {
+        Error::Crypto
+    }
+}
