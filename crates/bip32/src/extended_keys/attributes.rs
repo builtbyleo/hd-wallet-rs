5935c3dad278082f76bf2eec88a013ca50ef1c9a
builtbyleo <leonardpaturel@proton.me>
2026-06-22 11:36:43 +0200

    feat: use childnumber struct + constructor

--- crates/bip32/src/extended_keys/attributes.rs
+++ crates/bip32/src/extended_keys/attributes.rs
@@ -1,7 +1,34 @@
+use crate::extended_keys::errors::Error;
+
+// Max::u31 = 2^31
+const HARDENED_OFFSET: u32 = 1 << 31;
+
+#[derive(Clone)]
+pub struct ChildNumber(u32);
+
+impl ChildNumber {
+    pub fn normal(n: u32) -> Result<Self, Error> {
+        match n {
+            HARDENED_OFFSET.. => Ok(Self(n)),
+            _ => Err(Error::InvalidRange),
+        }
+    }
+    pub fn hardened(n: u32) -> Result<Self, Error> {
+        match n {
+            HARDENED_OFFSET.. => Ok(Self(n)),
+            _ => Err(Error::InvalidRange),
+        }
+    }
+
+    pub fn is_hardened(&self) -> bool {
+        self.0 >= HARDENED_OFFSET
+    }
+}
+
 #[derive(Clone)]
 pub struct ExtendedKeyAttrs {
     pub depth: u8,
     pub parent_fingerprint: [u8; 4],
-    pub child_number: u32,
+    pub child_number: ChildNumber,
     pub chain_code: [u8; 32],
 }
