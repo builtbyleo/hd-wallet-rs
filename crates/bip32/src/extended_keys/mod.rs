b7e6cccb405fbdabad0dd38b6df683f33934158d
builtbyleo <leonardpaturel@proton.me>
2026-06-23 19:32:58 +0200

    feat: add key size const

--- crates/bip32/src/extended_keys/mod.rs
+++ crates/bip32/src/extended_keys/mod.rs
@@ -1,7 +1,10 @@
 mod attributes;
+mod errors;
 mod private_key;
 mod public_key;
 
-pub use attributes::ExtendedKeyAttrs;
+pub use attributes::{ChildNumber, ExtendedKeyAttrs};
 pub use private_key::ExtPrivKey;
 pub use public_key::ExtPubKey;
+
+pub const KEY_SIZE: usize = 32;
