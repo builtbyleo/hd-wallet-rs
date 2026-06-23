db48168b9d798b9cd58fe9a7ae19f232300007ed
builtbyleo <leonardpaturel@proton.me>
2026-06-23 19:51:30 +0200

    feat add fingerprint

--- crates/bip32/src/extended_keys/public_key.rs
+++ crates/bip32/src/extended_keys/public_key.rs
@@ -1,9 +1,11 @@
 use k256::ecdsa::VerifyingKey;
+use ripemd::Ripemd160;
+use sha2::{Digest, Sha256};
 
 use crate::extended_keys::{ExtPrivKey, ExtendedKeyAttrs, KEY_SIZE};
 
 pub struct ExtPubKey {
-    pub_key: VerifyingKey,
+    public_key: VerifyingKey,
     attributes: ExtendedKeyAttrs,
 }
 
@@ -11,7 +13,7 @@ impl From<&ExtPrivKey> for ExtPubKey {
     fn from(xpriv: &ExtPrivKey) -> Self {
         let pub_key = VerifyingKey::from(xpriv.private_key());
         Self {
-            pub_key,
+            public_key: pub_key,
             attributes: xpriv.attributes().clone(),
         }
     }
@@ -19,10 +21,15 @@ impl From<&ExtPrivKey> for ExtPubKey {
 
 impl ExtPubKey {
     pub fn to_bytes(&self) -> [u8; KEY_SIZE + 1] {
-        self.pub_key
+        self.public_key
             .to_encoded_point(true)
             .as_bytes()
             .try_into()
             .expect("expected SEC1 key")
+    }
+
+    pub fn fingerprint(&self) -> [u8; 4] {
+        let pub_hash = Ripemd160::digest(Sha256::digest(self.to_bytes()));
+        pub_hash[..4].try_into().expect("pub hash truncated")
     }
 }
